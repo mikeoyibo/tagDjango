47b5c495e21870ea2bbd745a0107c9b270ef595e
Michael Oyibo <mikeoyibo@gmail.com>
2022-09-27 09:10:50 +0100

    Revised Version of Python Django Code

--- dic.py
+++
@@ -1,8 +0,0 @@
-myname = input("Please enter your name here: ")
-
-print("Hello, World!")
-
-print(myname)
-
-
-dict = {"hello": "what is your name"}
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+# from django.views.generic import TemplateView, ListView
+
+from .models import Post
 
 # Create your views here.
 
 
-class HomePageView(TemplateView):
+# class HomePageView(TemplateView):
 
+#     template_name = "home.html"
+
+
+# class AboutPageView(TemplateView):
+#     template_name = "about.html"
+
+
+class HomePageView(ListView):
+    model = Post
     template_name = "home.html"
-
-
-class AboutPageView(TemplateView):
-    template_name = "about.html"
